9749131bfd9aa7e014d56599a726a54282377c46
JulioDesar <cesarj598@gmail.com>
2022-05-12 19:12:12 -0300

    axios in tableCliente wip

--- src/Components/TableCliente/index.js
+++ src/Components/TableCliente/index.js
@@ -4,17 +4,21 @@ import "./style.css";
 import { FiSettings, FiTrash2 } from "react-icons/fi";
 
 function TableCliente() {
-
-    const [cliente, setCliente] = useState([]);
+    const [clientes, setClientes] = useState([]);
 
     async function carregarClientes() {
-        const resp = await axios.get('http://localhost:5000/cliente/');
-        setCliente(resp.data);
+        try {
+            await axios
+                .get("http://localhost:5000/cliente/")
+                .then((resp) => setClientes(resp.data));
+        } catch (err) {
+            console.error(err);
+        }
     }
 
     useEffect(() => {
         carregarClientes();
-    }, [])
+    }, []);
 
     return (
         <table className="table-cliente" cellPadding={0} cellSpacing={0}>
@@ -29,7 +33,7 @@ function TableCliente() {
                 </tr>
             </thead>
             <tbody>
-                {cliente.map(item =>
+                {clientes.map((item) => (
                     <tr key={item.id}>
                         <td>{item.id}</td>
                         <td>{item.nome}</td>
@@ -37,21 +41,21 @@ function TableCliente() {
                         <td>{item.telefone}</td>
                         <td>{item.endereco}</td>
                         <td>
-                        <div className="table-opcoes">
-                            <FiSettings
-                                size={18}
-                                style={{ cursor: "pointer" }}
-                                color="#1A365D"
-                            />
-                            <FiTrash2
-                                size={18}
-                                style={{ cursor: "pointer" }}
-                                color="#E53E3E"
-                            />
-                        </div>
-                    </td>
+                            <div className="table-opcoes">
+                                <FiSettings
+                                    size={18}
+                                    style={{ cursor: "pointer" }}
+                                    color="#1A365D"
+                                />
+                                <FiTrash2
+                                    size={18}
+                                    style={{ cursor: "pointer" }}
+                                    color="#E53E3E"
+                                />
+                            </div>
+                        </td>
                     </tr>
-                )}
+                ))}
             </tbody>
         </table>
     );

83226ecd22c0fd7e0c2ddee7f3b5902c848ab253
JulioDesar <cesarj598@gmail.com>
2022-05-11 21:22:36 -0300

    add axios

--- src/Components/TableCliente/index.js
+++ src/Components/TableCliente/index.js
@@ -1,8 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import { FiSettings, FiTrash2 } from "react-icons/fi";
 
 function TableCliente() {
+
+    const [cliente, setCliente] = useState([]);
+
+    async function carregarClientes() {
+        const resp = await axios.get('http://localhost:5000/cliente/');
+        setCliente(resp.data);
+    }
+
+    useEffect(() => {
+        carregarClientes();
+    }, [])
+
     return (
         <table className="table-cliente" cellPadding={0} cellSpacing={0}>
             <thead>
@@ -16,13 +29,14 @@ function TableCliente() {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>01</td>
-                    <td>Caio Santos</td>
-                    <td>caio.f@gmail.edu.br</td>
-                    <td>(11) 9 4355-8899</td>
-                    <td>R. Claudio Nunes</td>
-                    <td>
+                {cliente.map(item =>
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.nome}</td>
+                        <td>{item.email}</td>
+                        <td>{item.telefone}</td>
+                        <td>{item.endereco}</td>
+                        <td>
                         <div className="table-opcoes">
                             <FiSettings
                                 size={18}
@@ -36,28 +50,8 @@ function TableCliente() {
                             />
                         </div>
                     </td>
-                </tr>
-                <tr>
-                    <td>02</td>
-                    <td>Caio Santos</td>
-                    <td>caio.f@gmail.edu.br</td>
-                    <td>(11) 9 4355-8899</td>
-                    <td>R. Claudio Nunes</td>
-                    <td>
-                        <div className="table-opcoes">
-                            <FiSettings
-                                size={18}
-                                style={{ cursor: "pointer" }}
-                                color="#1A365D"
-                            />
-                            <FiTrash2
-                                size={18}
-                                style={{ cursor: "pointer" }}
-                                color="#E53E3E"
-                            />
-                        </div>
-                    </td>
-                </tr>
+                    </tr>
+                )}
             </tbody>
         </table>
     );
